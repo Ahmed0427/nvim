69acc4cc020f8671ed36085044ad77c20e332f13
Ahmed0427 <ahmedabdallah1090@gmail.com>
2025-05-26 15:24:22 +0300

    remove nvim-autopairs plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,11 +4,4 @@ return {
     -- Hints keybinds
     'folke/which-key.nvim',
   },
-  {
-    -- Autoclose parentheses, brackets, quotes, etc.
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    config = true,
-    opts = {},
-  },
 }
