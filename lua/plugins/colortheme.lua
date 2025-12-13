5776c5aabafa8746cc345ada44d6509066044c2d
ahmed0427 <ahmedabdallah1090@gmail.com>
2025-12-13 17:48:03 +0200

    use only rose pine

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,63 +1,39 @@
--- Helper function to set colorscheme and transparent background
+-- Helper function to set colorscheme and highlights
 local function setTheme(theme)
-  theme = theme or "rose-pine" -- Default theme
+  theme = theme or "rose-pine"
   vim.cmd.colorscheme(theme)
 
-  -- Make normal and floating window backgrounds transparent
+  -- Transparent backgrounds
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+  -- More visible Visual mode selection
+  vim.api.nvim_set_hl(0, "Visual", {
+    bg = "#403d52",
+    fg = "NONE",
+  })
 end
 
 -- Load theme AFTER plugins are ready
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
-    -- Options: "tokyonight", "gruvbox", "rose-pine"
     setTheme("rose-pine")
   end,
 })
 
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    config = function()
-      require("tokyonight").setup({
-        style = "storm",
-        transparent = false,
-        terminal_colors = true,
-        styles = {
-          comments = { italic = false },
-          keywords = { italic = false },
-          sidebars = "dark",
-          floats = "dark",
-        },
-      })
-    end,
-  },
-
-  {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true,
-        undercurl = true,
-        bold = true,
-        italic = { strings = false, comments = false, operators = false },
-        strikethrough = true,
-        inverse = true,
-      })
-    end,
-  },
-
-  {
     "rose-pine/neovim",
     name = "rose-pine",
+    lazy = false,
+    priority = 1000,
     config = function()
       require("rose-pine").setup({
         disable_background = true,
-        styles = { italic = false },
+        styles = {
+          italic = false,
+        },
       })
     end,
   },
