c5e0088f7ae69836201dd79952e6379f0d09d3bd
ahmed0427 <ahmedabdallah1090@gmail.com>
2026-03-05 19:01:26 +0200

    refactor: update plugin configurations for conform, telescope, and treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,27 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"python",
-				"go",
-				"c",
-				"cpp",
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"json",
-				"markdown",
-				"yaml",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "java",
+        "python",
+        "go",
+        "c",
+        "cpp",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "json",
+        "markdown",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
