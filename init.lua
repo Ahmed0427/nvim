2bb06b33aa57bd94fe90bfb28bb3aaad9344b376
ahmed0427 <ahmedabdallah1090@gmail.com>
2026-07-17 11:46:45 +0300

    update to harpoon2

--- init.lua
+++ init.lua
@@ -98,30 +98,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"ThePrimeagen/harpoon",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
-			vim.keymap.set("n", "<leader>ha", mark.add_file)
-			vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
-			vim.keymap.set("n", "<leader>hn", ui.nav_next)
-			vim.keymap.set("n", "<leader>hp", ui.nav_prev)
-			vim.keymap.set("n", "<leader>1", function()
-				ui.nav_file(1)
-			end)
-			vim.keymap.set("n", "<leader>2", function()
-				ui.nav_file(2)
-			end)
-			vim.keymap.set("n", "<leader>3", function()
-				ui.nav_file(3)
-			end)
-			vim.keymap.set("n", "<leader>4", function()
-				ui.nav_file(4)
-			end)
-		end,
-	},
-	{
 		"mbbill/undotree",
 		config = function()
 			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
@@ -129,6 +105,41 @@ require("lazy").setup({
 	},
 	{ "folke/which-key.nvim" },
 	{ "tpope/vim-sleuth" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+
+			vim.keymap.set("n", "<leader>ha", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<leader>hh", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+			vim.keymap.set("n", "<leader>hn", function()
+				harpoon:list():next()
+			end)
+			vim.keymap.set("n", "<leader>hp", function()
+				harpoon:list():prev()
+			end)
+
+			vim.keymap.set("n", "<leader>1", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<leader>2", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<leader>3", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<leader>4", function()
+				harpoon:list():select(4)
+			end)
+		end,
+	},
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "master",
